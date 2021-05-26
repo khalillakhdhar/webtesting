59e4a97140a706e1b6afa56ea8a6132a897641f3
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-26 10:20:11 +0100

    admin

--- src/webtesting/Admin.java
+++ src/webtesting/Admin.java
@@ -1,6 +1,22 @@
 package webtesting;
 
 public class Admin {
-String login,mdp;
+private String login,mdp;
+
+public String getLogin() {
+	return login;
+}
+
+public void setLogin(String login) {
+	this.login = login;
+}
+
+public String getMdp() {
+	return mdp;
+}
+
+public void setMdp(String mdp) {
+	this.mdp = mdp;
+}
 
 }
