fc2cfaef8bc81dbf6091cd51372fdded3ba1d851
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-26 10:26:04 +0100

    validateEnfant

---
+++ src/webtesting/testes/ValidationData.java
@@ -0,0 +1,26 @@
+package webtesting.testes;
+
+public class ValidationData {
+public boolean verif_nc(String nom)
+{
+	return nom.contains(" ");
+	
+
+}
+public boolean verifclass(String classe)
+{
+	if(classe.length()!=4)
+		return false;
+	else 
+		return true;
+
+
+}
+public double verif_note(String note)
+{
+	return Double.parseDouble(note);
+	
+
+}
+
+}
